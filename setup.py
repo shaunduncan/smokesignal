8062d1d8cf61172702d7b9f8df37d6e48f7bddc4
Shaun Duncan <shaun.duncan@gmail.com>
2014-03-25 07:27:45 -0400

    Version bump, and updated classifiers

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
-version = '0.3'
+version = '0.4'
 
 setup(name='smokesignal',
       version=version,
       description=("Simple python event signaling"),
       long_description=open('README.md').read(),
       classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Python Developers',
                    'License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: Implementation :: PyPy',
                    'Topic :: Software Development :: Libraries :: Python Modules',
                    'Topic :: Utilities'],
       keywords='python event signal signals signaling',
